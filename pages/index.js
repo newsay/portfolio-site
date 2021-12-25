fc598c7fc5314fe32aaa42533bcf66c3ee5d36c9
newsay <yaswens@gmail.com>
2021-12-25 17:48:27 -0500

    added hamburger

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+    
+    function myFunction(x) {
+        x.currentTarget.classList.toggle(styles.change);
+        console.log("TOgllign");
+    }
     return (
         <div className={styles.container}>
+            <div className={styles.hamburger} onClick={myFunction}>
+                <div className={styles.bar1}></div>
+                <div className={styles.bar2}></div>
+                <div className={styles.bar3}></div>
+            </div>
+
             <main className={styles.pages}>
                 <section>
                     <h2>Section 1</h2>
@@ -20,3 +31,4 @@ export default function Home() {
         </div>
     )
 }
+
