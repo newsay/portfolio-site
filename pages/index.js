bb2cc3e8d05a9ee7011ba1cf07a23aefd7801591
newsay <syaswen@gmail.com>
2022-01-24 18:27:49 -0500

    updated styling and links

--- pages/index.js
+++ pages/index.js
@@ -16,63 +16,66 @@ export default function Home() {
         </div>{" "}
         <div className={styles.dropdown__content}>
           <div>
-            <a> HOME </a>{" "}
+            <a href="#home"> HOME </a>{" "}
           </div>{" "}
           <div>
-            <a> WORK </a>{" "}
+            <a href="#work"> WORK </a>{" "}
           </div>{" "}
           <div>
-            <a> ABOUT </a>{" "}
+            <a href="#about"> ABOUT </a>{" "}
           </div>{" "}
           <div>
-            <a> CONTACT </a>{" "}
+            <a href="#contact"> CONTACT </a>{" "}
           </div>{" "}
         </div>{" "}
       </div>
       <main className={styles.pages}>
         <style>
-          @import
-          url(https://fonts.googleapis.com/css2?family=Dongle&display=swap);{" "}
-          @import
-          url(https://fonts.googleapis.com/css2?family=Dongle&family=Lexend:wght@500&display=swap);
-        </style>{" "}
-        <section className={styles.section__1}>
-          <h2> Shoshana Yaswen </h2> <h3> Software Developer </h3>{" "}
+          @import url(https://fonts.googleapis.com/css2?family=Dongle&family=Lexend:wght@500&family=Work+Sans:wght@100&display=swap);
+        </style>
+        <section className={styles.home}>
+        <title>Work in progress</title>
+
+          <img className={styles.cat} src="cat.png" />
+          <h2 id="home"> Shoshana Yaswen </h2> <h3> Software Developer </h3>{" "}
         </section>{" "}
-        <section className={styles.section__2}>
-          <h2> WORK </h2>{" "}
-        </section>{" "}
-        <section className={styles.section__3}>
-          <h2> ABOUT </h2>{" "}
-        </section>{" "}
-        <section className={styles.section__4}>
-            <h2> CONTACT </h2>
-            <label htmlFor="fname">Name</label>
-            <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Enter name"
-            ></input>
-            <label htmlFor="email">Email</label>
-            <input
-                type="text"
-                id="email"
-                name="email"
-                placeholder="Enter email"
-            ></input>
-                <label htmlFor="email">Message</label>
-                <input
-                type="text"
-                id="message"
-                name="message"
-                placeholder="Enter message"
-            ></input>
+      <section className={styles.work}>
+          <h2 id="work"> WORK </h2>
         </section>
-        <section className={styles.section__5}>
-          <h2> LAST SECTION </h2>{" "}
+        <section className={styles.about}>
+          <h2 id="about"> ABOUT </h2>{" "}
         </section>{" "}
-      </main>{" "}
+        <section className={styles.contact}>
+          <h2 id="contact"> CONTACT </h2>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter name"
+          ></input>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Enter email"
+          ></input>
+          <label htmlFor="email">Message</label>
+          <textarea>
+            Enter message
+          </textarea>
+        </section>
+        <section className={styles.links}>
+          <a className={styles.copyright}> <h2>© SHOSHANA YASWEN 2022</h2></a>
+          <div className={styles.linkbox}>
+            <a href="https://github.com/newsay/"><img className={styles.icon} src="github.png" /></a>
+            <a href="http://www.linkedin.com/in/newsay/"><img className={styles.icon} src="linkedin.png" /></a>
+            <a href="mailto:syaswen@gmail.com"><img className={styles.icon} src="mail.png" /></a>
+            <a href="https://twitter.com/y_swen/"><img className={styles.icon} src="twitter.png" /></a>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
